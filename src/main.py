40379102b61bb9bf4f1d4582014ed16fba2aae24
JustinEvans-github <53385587+JustinEvans-github@users.noreply.github.com>
2024-09-10 14:16:45 -0400

    help render in virtual env.

--- src/main.py
+++ src/main.py
@@ -11,6 +11,10 @@ from datetime import datetime, timedelta
 import argparse
 import sys
 
+from pyvirtualdisplay import Display
+display = Display(visible=0, size=(800, 800))  
+display.start()
+
 def parse_args():
     parser = argparse.ArgumentParser(description='action.yml arguments')
     parser.add_argument('--username', type=str, help='Username (case-insensitive)')
@@ -39,10 +43,10 @@ class archibus_scheduler():
 
         service = Service(ChromeDriverManager().install())
         chrome_options = webdriver.ChromeOptions() 
-
+        chrome_options.add_argument("--window-size=1200,1200")
         chrome_options.add_argument("--headless")  # Headless mode
         chrome_options.add_argument("--no-sandbox")  # Bypass OS security model
-        chrome_options.add_argument("--disable-dev-shm-usage")  # Overcome limited resource problems
+        #chrome_options.add_argument("--disable-dev-shm-usage")  # Overcome limited resource problems
         chrome_options.add_argument("--disable-gpu")  # Disable GPU rendering
         chrome_options.add_argument("--remote-debugging-port=9222")  # Required for headless mode
         chrome_options.add_argument('--allow-running-insecure-content')
